617b627af61f0653e487cf4330ca66a9eecde9ad
mgbachi_ugo <ugochukwu_mgbachi@yahoo.com>
2020-04-24 01:19:29 +0100

    Initial commit

---
+++ android/app/src/main/java/com/example/userfetch/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.userfetch;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:userfetch/api.dart';
+import 'package:userfetch/user.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: UsersListScreen(),
+    );
+  }
+}
+
+class UsersListScreen extends StatefulWidget {
+
+
+  @override
+  _UsersListScreenState createState() => _UsersListScreenState();
+}
+
+class _UsersListScreenState extends State<UsersListScreen> {
+  var users = new List<User>();
+
+
+  _getUsers() {
+    API.getUsers().then((response) {
+      setState(() {
+        Iterable list = json.decode(response.body);
+        users = list.map((model) => User.fromJson(model)).toList();
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//    throw UnimplementedError();
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+
+                title: Text(users[index].name),
+                subtitle: Text(users[index].email),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailScreen(),
+                        settings: RouteSettings(
+                          arguments: users[index],
+                        ),
+                      ));
+                }
+            );
+          }
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    throw UnimplementedError();
+    final User user = ModalRoute
+        .of(context)
+        .settings
+        .arguments;
+
+    return Scaffold(
+      body: new Center (
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.person,
+              color: Colors.blueAccent,
+              size: 300.0,
+            ),
+            SizedBox(height: 25.0),
+            new Text (
+               'Name: '+ user.name,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+            SizedBox(height: 10.0),
+            new Text (
+              ' Username: ' + user.username,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+            ),
+            SizedBox(height: 10.0),
+            new Text (
+              'Email: ' + user.email,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+            SizedBox(height: 10.0),
+            new Text (
+             'Phone: ' + user.phone,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
